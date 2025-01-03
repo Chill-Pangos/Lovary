4a54863c1c9c71de34f0690f3c18989165ee46f9
Minh Tuấn <minhtuan120704@gmail.com>
2025-01-03 09:03:36 +0700

    resolve all warnings

--- Lovary-FE/app/(tabs)/_layout.tsx
+++ Lovary-FE/app/(tabs)/_layout.tsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-import { View } from '@/components/Themed';
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+import { View } from "@/components/Themed";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
-
 function CustomAddButton() {
-  return (
-    <Pressable style={styles.addButton} >
-      
-    </Pressable>
-  );
+  return <Pressable style={styles.addButton}></Pressable>;
 }
 
 export default function TabLayout() {
@@ -31,15 +26,16 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: "Tab One",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
@@ -48,7 +44,7 @@ export default function TabLayout() {
                   <FontAwesome
                     name="info-circle"
                     size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
+                    color={Colors[colorScheme ?? "light"].text}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
@@ -58,27 +54,27 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-          name="about"
+        name="about"
         options={{
-            title: 'About',
-            tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          }}
+          title: "About",
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
       />
-      
+
       <Tabs.Screen
-          name="add"
-          options={{
-            tabBarButton: (props) => (
-              <CustomAddButton {...props}>
-                <Icon name="add" size={30} color="white" />
-              </CustomAddButton>
-            ),
-          }}
-        />
+        name="add"
+        options={{
+          tabBarButton: (props) => (
+            <CustomAddButton {...props}>
+              <Icon name="add" size={30} color="white" />
+            </CustomAddButton>
+          ),
+        }}
+      />
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
+          title: "Tab Two",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
@@ -89,18 +85,18 @@ export default function TabLayout() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   addButton: {
     top: -20,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     width: 60,
     height: 60,
     borderRadius: 30,
-    backgroundColor: '#8a2be2',
-    shadowColor: '#000',
+    backgroundColor: "#8a2be2",
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 10 },
     shadowOpacity: 0.25,
     shadowRadius: 3.5,
